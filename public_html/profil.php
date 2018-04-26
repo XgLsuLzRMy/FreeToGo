63f422894a152f9c461c1e3fed22675536799191
erohart <elodie.r-b@laposte.net>
2018-04-26 15:50:10 +0200

    php
    ajout du php pour modifier profil et pour la partie logements du propriétaire

--- public_html/profil.php
+++ public_html/profil.php
@@ -1,3 +1,29 @@
+<!-- ouverture de la session -->
+
+<?php
+session_start();
+
+// pour les tets a suprimer
+$_donnes['nom']='jean';
+$_donnes['prenom']='paul';
+$_donnes['age']='40';
+$_donnes['adresse']='rue budule chouette';
+$_donnes['description']='blabla ...Zzzz';
+//
+
+// connection a la base de donnée 
+$nomserveur='localhost'; //nom du seveur
+$nombd='freetogo'; //nom de la base de données
+$login='userfreetogo'; //login de l'utilisateur
+$mdp=''; // mot de passe
+$bd = new PDO('mysql:host='.$nomserveur.';dbname='.$nombd.'', $login);
+$reponse=$bd->query('SELECT * FROM client WHERE idClient ="'.$_SESSION['idClient'].'";');
+$donnees = $reponse->fetch();
+
+?>
+
+<!--page HTML -->
+
 <!doctype html>
 <html>
 <head>
@@ -12,35 +38,59 @@
 
   <!--la partie pour modifier le profil de l'utilisateur -->
 
-  <form>
+  <form action="profil.php" method="post">
     <h3> Votre profil </h3>
     <br/>
     <label>Nom : </label>
     <br/>
-    <input type="text" name="nom" id="nom" placeholder="saisissez votre nom"/>
+    <input type="text" name="nom" id="nom" placeholder="<?php echo $donnees['nom'];?>"/>
     <br/>
     <label>Prenom : </label>
     <br/>
-    <input type="text" name="preNom" id="nom" placeholder="saisissez votre prenom"/>
+    <input type="text" name="prenom" id="nom" placeholder="<?php echo $donnees['prenom'];?>"/>
     <br/>
     <label>age : </label>
     <br/>
-    <input type="text" name="age" id="nom" placeholder="saisissez votre age"/>
+    <input type="text" name="age" id="nom" placeholder="<?php echo $donnees['age'];?>"/>
     <br/>
     <label>Adresse : </label>
     <br/>
-    <input type="text" name="adresse" id="nom" placeholder="saisissez votre adresse"/>
+    <input type="text" name="adresse" id="nom" placeholder="<?php echo $donnees['adresse'];?>"/>
+    <br/>
+    <label>Telephone : </label>
+    <br/>
+    <input type="text" name="telephone" id="nom" placeholder="<?php echo $donnees['telephone'];?>"/>
     <br/>
     <label>Description : </label>
     <br/>
-    <textarea name="description" rows="10" cols="80" placeholder="saisissez votre description"></textarea>
+    <textarea name="description" rows="10" cols="80" placeholder="<?php echo $donnees['description'];?>"></textarea>
     <br/>
     <label>Photo de profil : </label>
-    <br/>
-    <input type="file" name="photo" id="photo"/>
+    <br/> 
+    <?php if (empty ($donnees['photoProfil'])){
+               echo "<input type=\"file\" name=\"photoProfil\" id=\"photo\"/>"; }
+          else {echo "<img src=\"".$donnees['photoProfil']."\"/>";}
+     ?>
     <br/>
 
     <input type="submit" class="bouton" name="Enregistrer" value="Enregistrer"/>
+    <?php
+if (isset($_POST['Enregistrer'])) {
+
+    $requete = $bdd->prepare('UPDATE Client SET nom = :nom, prenom = :prenom, age = :age, adresse = :adresse, telephone = :telephone, description = :description, photoProfil = :photoProfil WHERE idClient = :idClient');
+
+    $requete->execute(array(
+        'nom' => $_POST['nom'],
+        'prenom' => $_POST['prenom'],
+        'age' => $_POST['age'],
+        'adresse' => $_POST['adresse'],
+        'telephone' => $_POST['telephone'],
+        'description' => $_POST['description'],
+        'photoProfil' => $_POST['photoProfil'],
+        'idClient' => $donnees['idClient'],
+    )); }
+
+?>
   </form>
 
   <!--la partie pour ajouter un logement a louer sur le site -->
@@ -48,21 +98,36 @@
   <section>
     <h3> Vos logements </h3>
     <br/>
-    <!-- il faudra faire un tableau en PHP avec un tableau du modele si dessous :
-    <thead> <td colspan="2"> Logements </thead>
-    <table>
-    <tbody>
-    <tr> <td> logement 1 </td>
-    <td> <input type="submit" class="bouton" name="Enregistrer" value="voir"/> </td>
-  </tr>
-  <tr> <td> logement 2 </td>
-  <td> <input type="submit" class="bouton" name="Enregistrer" value="voir"/> </td>
-</tr>
-</tbody>
-</table>
--->
+  <?php
+      $reponse = $bd->query('SELECT * FROM logement where idClient="'.(string)$_SESSION['idClient'].'"');
+
+      $num = 1;
+      echo"<table>";
+      while ($donnees = $reponse->fetch()) //tant qu'il y a des lignes de logements
+      {
+        echo "
+        <tr>
+        <th></th>
+        <th>Nom du logement</th>
+        <th>Localisation</th>
+        <th>Prix</th>
+        <th>Voir</th>
+        </tr>
+        <tr>
+        <td>".$num."</td>
+        <td>Logement ".$donnees["nomLogement"]."</td>
+        <td>".$donnees["ville"]."</td>
+        <td>".$donnees["prix"]."</td>
+        <td>
+        <button onclick=\"location.href='pageLogement.php?idLogement=".(string)$donnees["idLogement"]."'\" type=\"button\">VOIR</button>
+        </td>
+        </tr>";
+        $num = $num + 1;
+      }
+      echo "</table>";
+  ?>
 <br/>
-<form action="ajoutlogement.php">
+<form action="ajoutlogement.php" method="post">
   <input type="submit" class="bouton" name="lienAjoutLogement" value="Ajouter un logement"/>
 </form>
 </section>
