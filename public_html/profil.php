2972d59e377b8a95ba13871e34c9f07ab45cbe1f
XgLsuLzRMy <lucas.ramage@insa-rouen.fr>
2018-04-28 20:08:30 +0200

    Corrections profil.php

--- public_html/profil.php
+++ public_html/profil.php
@@ -17,9 +17,10 @@ $nombd='freetogo'; //nom de la base de données
 $login='userfreetogo'; //login de l'utilisateur
 $mdp=''; // mot de passe
 $bd = new PDO('mysql:host='.$nomserveur.';dbname='.$nombd.'', $login);
-$reponse=$bd->query('SELECT * FROM client WHERE idClient ="'.$_SESSION['idClient'].'";');
-$donnees = $reponse->fetch();
-
+if (isset($_SESSION['idClient'])){
+  $reponse=$bd->query('SELECT * FROM client WHERE idClient ="'.$_SESSION['idClient'].'";');
+  $donnees = $reponse->fetch();
+}
 ?>
 
 <!--page HTML -->
@@ -34,7 +35,7 @@ $donnees = $reponse->fetch();
 </head>
 <body>
   <?php include('include/header.html'); ?>
-<div class="main">
+
 
   <!--la partie pour modifier le profil de l'utilisateur -->
 
@@ -68,18 +69,18 @@ $donnees = $reponse->fetch();
     <label>Photo de profil : </label>
     <br/>
     <?php if (empty ($donnees['photoProfil'])){
-               echo "<input type=\"file\" name=\"photoProfil\" id=\"photo\"/>"; }
-          else {echo "<img src=\"".$donnees['photoProfil']."\"/>";}
-     ?>
+      echo "<input type=\"file\" name=\"photoProfil\" id=\"photo\"/>";
+    }
+    else {echo "<img src=\"".$donnees['photoProfil']."\"/>";}
+    ?>
     <br/>
 
     <input type="submit" class="bouton" name="Enregistrer" value="Enregistrer"/>
     <?php
-if (isset($_POST['Enregistrer'])) {
+    if (isset($_POST['Enregistrer'])) {
+      $requete = $bd->prepare('UPDATE client SET `nom` = "nom", `prenom` = "prenom", `age` = "age", `adresse` = "adresse", `telephone` = "telephone", `description` = "description", `photoProfil` = "photoProfil" WHERE `idClient` = idClient');
 
-    $requete = $bd->prepare('UPDATE client SET nom = :nom, prenom = :prenom, age = :age, adresse = :adresse, telephone = :telephone, description = :description, photoProfil = :photoProfil WHERE idClient = :idClient');
-
-    $requete->execute(array(
+        $requete->execute(array(
         'nom' => $_POST['nom'],
         'prenom' => $_POST['prenom'],
         'age' => $_POST['age'],
@@ -88,17 +89,17 @@ if (isset($_POST['Enregistrer'])) {
         'description' => $_POST['description'],
         'photoProfil' => $_POST['photoProfil'],
         'idClient' => $donnees['idClient'],
-    )); }
+      )); }
 
-?>
-  </form>
+      ?>
+    </form>
 
-  <!--la partie pour ajouter un logement a louer sur le site -->
+    <!--la partie pour ajouter un logement a louer sur le site -->
 
-  <section>
-    <h2> Vos logements </h2>
-    <br/>
-  <?php
+    <section>
+      <h2> Vos logements </h2>
+      <br/>
+      <?php
       $reponse = $bd->query('SELECT * FROM logement where idClient="'.(string)$_SESSION['idClient'].'"');
 
       $num = 1;
@@ -125,61 +126,63 @@ if (isset($_POST['Enregistrer'])) {
         $num = $num + 1;
       }
       echo "</table>";
-  ?>
-<br/>
-<form action="ajoutlogement.php" method="post">
-  <input type="submit" class="bouton" name="lienAjoutLogement" value="Ajouter un logement"/>
-</form>
-</section>
+      ?>
+      <br/>
+      <form action="ajoutlogement.php" method="post">
+        <input type="submit" class="bouton" name="lienAjoutLogement" value="Ajouter un logement"/>
+      </form>
+    </section>
 
-<!-- la partie pour voir les réservations -->
+    <!-- la partie pour voir les réservations -->
 
-<section>
-  <h2> Vos reservations </h2>
-  <?php
-      $reponse = $bd->query('SELECT * FROM reserver where idClient="'.(string)$_SESSION['idClient'].'"');
-      $num = 1;
-      echo"<table>";
-      while ($donnees1 = $reponse->fetch()) //tant qu'il y a des lignes de logements
-      {
-        $reponse2 = $bd->query('SELECT * FROM logement where idLogement="'.(string)$_donnees['idLogement'].'"');
-        $donnees = $reponse2->fetch();
-        echo "
-        <tr>
-        <th></th>
-        <th>Nom du logement</th>
-        <th>Localisation</th>
-        <th>Prix</th>
-        <th>nom du propriétaire</th>
-        <th>Voir</th>
-        <th>Commenter</th>
-        </tr>
-        <tr>
-        <td>".$num."</td>
-        <td>Logement ".$donnees["nomLogement"]."</td>
-        <td>".$donnees["ville"]."</td>
-        <td>".$donnees["prix"]."</td>
-        <td>Mr/MMe ".$donnees["nom"]."</td>
-        <td>
-        <button onclick=\"location.href='pageLogement.php?idLogement=".(string)$donnees["idLogement"]."'\" type=\"button\">VOIR</button>
-        </td>
-        <td>
-        <form action=\"profil.php\" method=\"post\">
+    <section>
+      <h2> Vos reservations </h2>
+      <?php
+      if (isset($_SESSION['idClient'])){
+        $reponse = $bd->query('SELECT * FROM reserver where idClient="'.(string)$_SESSION['idClient'].'"');
+        $num = 1;
+        echo"<table>";
+        while ($donnees1 = $reponse->fetch()) //tant qu'il y a des lignes de logements
+        {
+          $reponse2 = $bd->query('SELECT * FROM logement where idLogement="'.(string)$_donnees['idLogement'].'"');
+          $donnees = $reponse2->fetch();
+          echo "
+          <tr>
+          <th></th>
+          <th>Nom du logement</th>
+          <th>Localisation</th>
+          <th>Prix</th>
+          <th>nom du propriétaire</th>
+          <th>Voir</th>
+          <th>Commenter</th>
+          </tr>
+          <tr>
+          <td>".$num."</td>
+          <td>Logement ".$donnees["nomLogement"]."</td>
+          <td>".$donnees["ville"]."</td>
+          <td>".$donnees["prix"]."</td>
+          <td>Mr/MMe ".$donnees["nom"]."</td>
+          <td>
+          <button onclick=\"location.href='pageLogement.php?idLogement=".(string)$donnees["idLogement"]."'\" type=\"button\">VOIR</button>
+          </td>
+          <td>
+          <form action=\"profil.php\" method=\"post\">
           <input type= \"button\" class=\"bouton\" name=\"commenter\" value=\"ajouter un commentaire\"/>
           </td> <input type=\"text\" name=\"cache\" style=\"visible:off\" value=\"".$donnees['idLogement']."\"/>
-        </form>
+          </form>
 
-        </tr>";
-        $num = $num + 1;
+          </tr>";
+          $num = $num + 1;
+        }
+        echo "</table>";
       }
-      echo "</table>";
-?>
+      ?>
 
-<?php
-// faire un commentaire :
+      <?php
+      // faire un commentaire :
 
       if (isset($_POST['commenter'])) {
-        $reponse=$bd->query('SELECT * FROM logement WHERE idLogement ="'.$_SESSION['cache'].'";');
+        $reponse=$bd->query('SELECT * FROM Logment WHERE idLogement ="'.$_SESSION['cache'].'";');
         $donnees = $reponse->fetch();
 
         echo "  <label>Commenter le logement ".$donnes['nomLogement'] ." : </label>
@@ -187,33 +190,32 @@ if (isset($_POST['Enregistrer'])) {
         <textarea name=\"description\" rows=\"10\" cols=\"80\" placeholder=\"Ecrire un commentaire\"></textarea>
         <br/>
         <form action=\"profil.php\" method=\"post\">
-          <input type=\"submit\" class=\"bouton\" name=\"poster\" value=\"poster le commentaire\"/>
+        <input type=\"submit\" class=\"bouton\" name=\"poster\" value=\"poster le commentaire\"/>
         </form>";
         if (isset($_POST['poster'])) {
 
-          $requete = $bd->prepare('INSERT INTO commentaire VALUES comment = :commenter, idCommentaire = :idCommentaire, idClient = :idClient');
+          $requete = $bd->prepare('INSERT INTO Commentaire VALUES comment = :commenter, idCommentaire = :idCommentaire, idClient = :idClient');
 
           $requete->execute(array(
             'commenter' => $_POST['commenter'],
             'idClient' => $_SESSION['idClient'],
             'idCommentaire' => $com,
-        ));
-        $reponse2=$bd->query('SELECT idCommentaire FROM Commentaire');
-        $com = 0;
-        while ($donnees2 = $reponse2->fetch()) //tant qu'il y a des lignes de logements
-        {
-         $com = $com + 1 ;
+          ));
+          $reponse2=$bd->query('SELECT idCommentaire FROM Commentaire');
+          $com = 0;
+          while ($donnees2 = $reponse2->fetch()) //tant qu'il y a des lignes de logements
+          {
+            $com = $com + 1 ;
+          }
         }
       }
-    }
-  ?>
+      ?>
 
 
-  <!-- il faudra faire un bouton qui permet d'ajouter un commentaire  : -->
+      <!-- il faudra faire un bouton qui permet d'ajouter un commentaire  : -->
 
 
-</section>
-</div>
+    </section>
 
-</body>
-</html>
+  </body>
+  </html>
