33871dfa8d7bfe19ab291cbc3c81d63b1d969762
jcatelain <jeremy.catelain@insa-rouen.fr>
2018-05-14 23:01:33 +0200

    ..

--- public_html/ajoutlogement.php
+++ public_html/ajoutlogement.php
@@ -2,7 +2,6 @@
 <html lang="fr">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <link rel="stylesheet" type="text/css" href="css/ajoutLogement.css" />
   <link rel="stylesheet" type="text/css" href="css/common.css" />
   <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
   <title>FreeToGo</title>
---
+++ public_html/profil(copie).php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link rel="stylesheet" type="text/css" href="css/common.css" />
+  <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+  <title>FreeToGo</title>
+</head>
+<body>
+  <?php include('include/header.html'); require_once('include/fonctions.php');
+  ouvrirSession();
+  if (isset($_SESSION['idClient'])){
+    $bd = seConnecterABD();
+    $reponse=$bd->query('SELECT * FROM client WHERE idClient ="'.$_SESSION['idClient'].'";');
+    $donnees = $reponse->fetch();
+  }
+  ?>
+  <div class="main">
+    <?php
+    if (isset($_GET['succes'])){
+      afficherMessageSucces("Les modifications ont bien été enregistrées!");
+    }
+    if (isset($_GET['mailError'])){
+      afficherMessageErreur("Adresse mail non valide ou déjà utilisée par un utilisateur");
+    }
+    if (isset($_GET['telError'])){
+      afficherMessageErreur("numero de telephone non valide");
+    }
+    ?>
+    <!--la partie pour modifier le profil de l'utilisateur -->
+    <form  method="post" enctype="multipart/form-data">
+      <div class="ligne">
+        <div class="colonne" >
+          <h2> Votre profil </h2>
+          <br/>
+
+          <label>Nom : </label>
+          <br/>
+          <input type="text" name="nom" value="<?php if(isset($donnees)){echo $donnees['nom'];}?>" required />
+          <br/>
+          <label>Prenom : </label>
+          <br/>
+          <input type="text" name="prenom" value="<?php if(isset($donnees)){echo $donnees['prenom'];}?>"/>
+          <br/>
+          <label>age : </label>
+          <br/>
+          <input type="number" name="age" value="<?php if(isset($donnees)){echo $donnees['age'];}?>"/>
+          <br/>
+          <label>Adresse : </label>
+          <br/>
+          <input type="text" name="adresse" value="<?php if(isset($donnees)){echo $donnees['adresse'];}?>"/>
+          <br/>
+        </div>
+        <div class="colonne" style="margin-top: 50px;">
+          <label>Telephone : </label>
+          <br/>
+          <input type="text" name="telephone" value="<?php if(isset($donnees)){echo $donnees['telephone'];}?>"/>
+          <br/>
+          <label>Email : </label>
+          <br/>
+          <input type="text" name="mail" value="<?php if(isset($donnees)){echo $donnees['mail'];}?>" required />
+          <br/>
+          <label>Description : </label>
+          <br/>
+          <textarea name="description" rows="10" cols="80"><?php if(isset($donnees)){echo $donnees['description'];}?></textarea>
+          <br/>
+        </div>
+        <div class="colonne" style="margin-top: 50px;" >
+          <label>Photo de profil : </label>
+          <br/>
+          <?php
+          if(isset($donnees)){
+            $data=$donnees;
+          }else{
+            $data=NULL;
+          }
+          $photo=gererPhoto($data, 'photoProfil', "./images/profil_default.png");
+          echo '<input type="file" name="photoProfil" id="photo"/>';
+          echo '<img alt="photo de profil" class="photo_profil"  src="'.$photo.'"/>';
+          ?>
+          <br/>
+        </div>
+      </div>
+      <input type="submit" class="bouton" name="Enregistrer" value="Enregistrer"/>
+    </form>
+
+    <?php
+    if (isset($_POST['Enregistrer'])) {
+
+      $mailChange = $_POST['mail']!=$donnees['mail'];
+
+      $nom = calculChamps("nom",$donnees);
+      $prenom = calculChamps("prenom",$donnees);
+      $age = (int)calculChamps("age",$donnees);
+      $adresse = calculChamps("adresse",$donnees);
+      $telephone = calculChamps("telephone",$donnees);
+      $mail = calculChamps("mail",$donnees);
+      $description = calculChamps("description",$donnees);
+      $name="";
+      $dossier = './images';
+
+      if($mail != NULL && $mailChange){
+        $patternMail = '/^[a-z0-9_.-]+@[a-z0-9._-]+\.[a-z0-9]+$/i';
+        if(!preg_match($patternMail, $mail)){
+          echo '<script>window.location.replace("./profil.php?mailError");</script>';
+          die();
+        }elseif (mailExiste($mail)) {
+          echo '<script>window.location.replace("./profil.php?mailError");</script>';
+          die();
+        }
+      }
+      if($telephone != NULL){
+        $patternTel = '/^0[0-9]{9}$/';
+        $patternTel2 = '/^0[0-9]\.[0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]$/';
+        if(!(preg_match($patternTel, $telephone) || preg_match($patternTel2, $telephone))){
+          echo '<script>window.location.replace("./profil.php?telError");</script>';
+          die();
+        }
+      }
+
+      if(file_exists($_FILES['photoProfil']['tmp_name']) || is_uploaded_file($_FILES['photoProfil']['tmp_name'])) {
+        // supprimer l'image précédente
+        if (file_exists($donnees['photoProfil'])){
+          unlink($donnees['photoProfil']);
+        }
+        //copier l'image chargée dans le dossier image
+        $dossier = './images';
+        $tmp_name = $_FILES['photoProfil']["tmp_name"];
+        $name = "profil_".$_SESSION['idClient'].".".pathinfo($_FILES['photoProfil']['name'], PATHINFO_EXTENSION);
+        move_uploaded_file($tmp_name, "$dossier/$name");
+        if($name==""){
+          $name="profil_default.png";
+          $photoProfil="$dossier/$name";
+        }
+        $photoProfil="$dossier/$name";
+      }elseif($donnees["photoProfil"]!=NULL){
+        $photoProfil=$donnees["photoProfil"];
+      }
+      //$photoProfil = calculChamps('photoProfil',$donnees);
+      $requete = $bd->prepare('UPDATE client SET nom = :nom, prenom = :prenom, age = :age, adresse = :adresse, telephone = :telephone, mail =:mail, description = :description, photoProfil = :photoProfil WHERE idClient = :idClient');
+      $requete->execute(array(
+        'nom' => $nom,
+        'prenom' => $prenom,
+        'age' => $age,
+        'adresse' => $adresse,
+        'telephone' => $telephone,
+        'mail' =>  $mail,
+        'description' => $description,
+        'photoProfil' => $photoProfil,
+        'idClient' => $donnees['idClient']
+      ));
+      echo '<script>window.location.replace("./profil.php?succes");</script>';
+      die();
+    }
+    ?>
+
+    <form action="connexion.php" method="post">
+      <input type="submit" value="supprimer votre compte" name="suppressionCompte" style="background-color:red;padding: 0.5%;color: antiquewhite;font-weight: bold;" />
+    </form>
+
+    <!--la partie pour ajouter un logement a louer sur le site -->
+
+    <section>
+      <h2> Vos logements </h2>
+      <br/>
+      <?php
+      if (isset($_SESSION['idClient'])){
+        $reponse = $bd->query('SELECT * FROM logement where idClient="'.(string)$_SESSION['idClient'].'"');
+        $num = 1;
+        echo '<table class="tableaux">';
+        echo '
+        <tr>
+        <th></th>
+        <th>Nom du logement</th>
+        <th>Localisation</th>
+        <th>Prix</th>
+        <th>Voir</th>
+        </tr>';
+        while ($donnees = $reponse->fetch()) //tant qu'il y a des lignes de logements
+        {
+          echo '
+          <tr>
+          <td>'.$num.'</td>
+          <td>Logement '.$donnees["nomLogement"].'</td>
+          <td>'.$donnees["ville"].'</td>
+          <td>'.$donnees["prix"].'</td>
+          <td>
+          <button onclick="location.href=\'pageLogement.php?idLogement='.(string)$donnees["idLogement"].'\'" type="button" class="bouton">Voir</button>
+          </td>
+          </tr>';
+          $num = $num + 1;
+        }
+        echo "</table>";
+      }
+      ?>
+      <br/>
+      <form action="ajoutlogement.php" method="post">
+        <input type="submit" class="bouton" name="lienAjoutLogement" value="Ajouter un logement"/>
+      </form>
+    </section>
+
+    <!-- la partie pour voir les réservations -->
+
+    <section>
+      <h2> Vos reservations </h2>
+      <?php
+      if (isset($_SESSION['idClient'])){
+        $reponse = $bd->query('SELECT * FROM reserver where idClient="'.(string)$_SESSION['idClient'].'"');
+        $num = 1;
+        echo '<table class="tableaux">';
+        echo '
+        <tr>
+        <th></th>
+        <th>Nom du logement</th>
+        <th>Localisation</th>
+        <th>Prix</th>
+        <th> Date début de reservation </th>
+        <th> Date fin de reservation </th>
+        <th>Voir</th>
+        <th>Commenter</th>
+        </tr>';
+        while ($donnees1 = $reponse->fetch()) //tant qu'il y a des lignes de logements
+        {
+          $reponse2 = $bd->query('SELECT * FROM logement where idLogement="'.(string)$donnees1['idLogement'].'"');
+          $donnees = $reponse2->fetch();
+          echo '
+          <tr>
+          <td>'.$num.'</td>
+          <td>Logement '.$donnees["nomLogement"].'</td>
+          <td>'.$donnees["ville"].'</td>
+          <td>'.$donnees["prix"].'</td>
+          <td> '.(string)$donnees1['dateDebut'].' </td>
+          <td>'.(string)$donnees1['dateFin'].' </td>
+          <td>
+          <button onclick="location.href=\'pageLogement.php?idLogement='.(string)$donnees1["idLogement"].'\'" type="button" class="bouton">Voir</button>
+          </td>
+          <td>';
+          $date = date('Y-m-d');
+          $dateactuelle = date_create( $date);
+          if($donnees1['dateFin']>$dateactuelle){
+            echo
+          '
+          <button onclick="location.href=\'commentaire.php?idLogement='.(string)$donnees1["idLogement"].'\'" type="button" class="bouton">Commenter</button>
+          </tr>';
+          }
+          $num = $num + 1;
+        }
+        echo "</table>";
+      }
+      ?>
+    </section>
+  </div>
+</body>
+</html>
